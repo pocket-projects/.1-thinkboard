e653c1d7dec1375471c0f5e1585598a6443e503f
tharundevaraja <tharunrandiv@gmail.com>
2025-07-07 23:05:47 +0530

    Initialized frontend and developed main page UI for Thinkboard

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,80 @@
+import {useState} from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import { useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
+
+
+const HomePage = () => {
+    const [isRateLimited, setIsRateLimited] = useState(false);
+    const [notes, setNotes] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try{
+                const res = await axios.get("http://localhost:5001/api/notes");
+                console.log(res.data);
+                setNotes(res.data);
+                setIsRateLimited(false);
+            } catch (error) {
+                console.log("Error fetching notes:", error);
+                if(error.response && error.response.status === 429){ //ratelimited- 429
+                    setIsRateLimited(true);
+                } else {
+                    toast.error("Failed to load notes")
+                }
+            } finally {
+                setLoading(false); 
+            }
+        };
+
+        fetchNotes();
+    },[])
+     
+  return (
+    <div className="min-h-screen">
+        <Navbar />
+
+        {isRateLimited && <RateLimitedUI />}
+
+        <div className="max-w-7xl mx-auto p-4 mt-6">
+            {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+            {notes.length > 0 && !isRateLimited && (
+                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {notes.map(note => (
+                        <div>
+                            <NoteCard key={note._id} note={note} />
+                        </div>
+                    ))}
+
+                 </div>
+            )}
+
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
+
+
+//Fetch notes from the backend Example 
+
+    // useEffect(() => {
+    //     const fetchNotes = async () => {
+    //         try{
+    //             const res = await fetch("http://localhost:5001/api/notes");
+    //             const data = await res.json();
+    //             console.log(data);
+    //         } catch (error) {
+    //             console.log("Error fetching notes:", error);
+    //         }
+    //     };
+
+    //     fetchNotes();
+    // },[])
+     
